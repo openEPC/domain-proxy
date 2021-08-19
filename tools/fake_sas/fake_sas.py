634eaaee31c9db16e95ed8ca05cbbaca3d11a1bc
Kuba Marciniszyn <kuba@freedomfi.com>
2021-08-19 15:56:19 +0200

    Fix fake sas spectrum inquiry response: available channel was an object not an array

--- tools/fake_sas/fake_sas.py
+++ tools/fake_sas/fake_sas.py
@@ -119,14 +119,14 @@ class FakeSas(sas_interface.SasInterface):
     for req in request['spectrumInquiryRequest']:
       response['spectrumInquiryResponse'].append({
           'cbsdId': req['cbsdId'],
-          'availableChannel': {
+          'availableChannel': [{
               'frequencyRange': {
                   'lowFrequency': 3620000000,
                   'highFrequency': 3630000000
               },
               'channelType': 'GAA',
               'ruleApplied': 'FCC_PART_96'
-          },
+          }],
           'response': self._GetSuccessResponse()
       })
     return response
